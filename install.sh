d5a90b15e996a224ba8b5fc10a7b77d187eeb6e2
acebrian <acebrian@paradigmadigital.com>
2017-09-12 16:07:08 +0200

    install changed

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ echo "============================="
 
 VERSION=$(curl -L -s -S https://beta.requarks.io/api/version/stable)
 
+echo "-> Background images"
+curl "https://www.w3schools.com/css/img_lights.jpg" -o ./wiki/assets/images/bg_1.jpg
+
+
 echo "-> Fetching latest build..."
 curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C ./wiki
 
@@ -13,6 +17,6 @@ echo "-> Fetching dependencies..."
 curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/node_modules.tar.gz | tar xz -C ./wiki
 
 echo "-> Background images"
-curl "https://www.w3schools.com/css/img_lights.jpg" -o ./wiki/assets/images/bc_1.jpg
+curl "https://www.w3schools.com/css/img_lights.jpg" -o ./wiki/assets/images/bg_1.jpg
 
 echo "-> Installation Complete"
